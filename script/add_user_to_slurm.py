20a68e9eda3c43742cf0dfe9aa61d53d27ebbc29
kerernwwerr <kerwintsai@pm.me>
2024-03-23 10:44:12 +0000

    Update script.

--- script/add_user_to_slurm.py
+++ script/add_user_to_slurm.py
@@ -4,8 +4,7 @@ def add_user_to_slurm_account(username, account='lab'):
 
     # USer sacctmgr to add user to an exist slurm account.
     command = f"sacctmgr add user {username} Account={account} --immediate"
-    
-    # 执行命令
+
     try:
         result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
         if result.stderr:
@@ -16,6 +15,5 @@ def add_user_to_slurm_account(username, account='lab'):
         print(f"Falt a slurm contorl: {e}")
     
 if __name__ == "__main__":
-    # 从用户输入获取用户名
     username = input("Username: ")
     add_user_to_slurm_account(username)
